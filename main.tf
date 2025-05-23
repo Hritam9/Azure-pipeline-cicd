8380258228616521a46e98aa941f87f4b3a0bd4c
Hritam Jaiwal <43675990+Hritam9@users.noreply.github.com>
2025-05-23 17:42:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,102 +1,138 @@
-provider "azurerm" {
-  features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-funcapps"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-funcapps"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-  service_endpoints    = ["Microsoft.Web"]
-}
-
-resource "azurerm_private_dns_zone" "dns" {
-  name                = "privatelink.azurewebsites.net"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "dnslink" {
-  name                  = "vnet-link"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.dns.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-  registration_enabled  = false
-}
-
-resource "azurerm_app_service_plan" "plan" {
-  name                = "app-plan-premium"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  kind                = "linux"
-  reserved            = true
-
-  sku {
-    tier = "PremiumV2"
-    size = "EP1"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100.0"
+    }
   }
 }
 
-resource "azurerm_storage_account" "sa" {
-  for_each = var.function_apps
+provider "azurerm" {
+  features = {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
 
-  name                     = each.value.storage
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-devops-demo"
+  location = "East US"
+}
+
+# Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                     = "devopsdemostgfunc"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
+# App Service Plan (Premium)
+resource "azurerm_service_plan" "plan" {
+  name                = "asp-devops-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "EP1"
+  worker_count        = 1
+}
+
+# Virtual Network and Subnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-devops-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-devops-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  service_endpoints    = ["Microsoft.Web"]
+  delegations {
+    name = "delegation"
+    service_delegation {
+      name = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
+# Private DNS Zone
+resource "azurerm_private_dns_zone" "dns" {
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dnslink" {
+  name                  = "dnslink"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+}
+
+# Function Apps definition
+locals {
+  function_apps = {
+    "funcapp1" = {
+      name     = "funcapp-demo-1"
+    },
+    "funcapp2" = {
+      name     = "funcapp-demo-2"
+    }
+  }
+}
+
+# Function Apps
 resource "azurerm_linux_function_app" "funcapps" {
-  for_each = var.function_apps
+  for_each = local.function_apps
 
   name                       = each.value.name
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
-  service_plan_id            = azurerm_app_service_plan.plan.id
-  storage_account_name       = azurerm_storage_account.sa[each.key].name
-  storage_account_access_key = azurerm_storage_account.sa[each.key].primary_access_key
+  service_plan_id            = azurerm_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
 
   site_config {
-    linux_fx_version = "Python|3.10"
+    application_stack {
+      python_version = "3.10"
+    }
   }
 
   identity {
     type = "SystemAssigned"
   }
+
+  functions_extension_version = "~4"
 }
 
-resource "azurerm_app_service_virtual_network_swift_connection" "vnetint" {
-  for_each       = var.function_apps
-  app_service_id = azurerm_linux_function_app.funcapps[each.key].id
+# VNet Integration (via subnet delegation)
+resource "azurerm_app_service_virtual_network_swift_connection" "integration" {
+  for_each = azurerm_linux_function_app.funcapps
+
+  app_service_id = each.value.id
   subnet_id      = azurerm_subnet.subnet.id
 }
 
-resource "azurerm_private_endpoint" "pep" {
-  for_each            = var.function_apps
-  name                = "${each.value.name}-pep"
+# Private Endpoints
+resource "azurerm_private_endpoint" "pe" {
+  for_each = azurerm_linux_function_app.funcapps
+
+  name                = "${each.key}-pe"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   subnet_id           = azurerm_subnet.subnet.id
 
   private_service_connection {
-    name                           = "${each.value.name}-psc"
-    private_connection_resource_id = azurerm_linux_function_app.funcapps[each.key].id
+    name                           = "${each.key}-privatesc"
+    private_connection_resource_id = each.value.id
     subresource_names              = ["sites"]
     is_manual_connection           = false
   }
